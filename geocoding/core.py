08a887741b308789512e53beb5dbf3741e0ac156
Casey Conger <cconger@aflatworld.com>
2018-12-14 17:21:04 -0500

    Made progress on joining the company location database and the water resource data.

--- geocoding/core.py
+++ geocoding/core.py
@@ -1,7 +1,7 @@
 ###############################################################################
 ###############################################################################
 ##                                                                           ##
-##  water_resource.py                                                        ##
+##  core.py                                                                  ##
 ##  Date Created: 2018/12/12                                                 ##
 ##                                                                           ##
 ##  This module contains a collection of functions for connecting business   ##
@@ -13,7 +13,10 @@
 
 import geopandas
 import fiona
+import os
+
 from geopy.geocoders import GoogleV3
+from shapely import Point
 
 ###############################################################################
 ## CONSTANTS ##################################################################
@@ -21,6 +24,35 @@ from geopy.geocoders import GoogleV3
 
 #TODO: move this to a configuration file
 GOOGLE_GEOCODE_API_KEY = 'AIzaSyAnM0TuA8nP9NUUfCeqb-JBjk93eD2Bdlw'
+GDBS = {
+    'aqueduct_global': os.path.join(
+        os.getcwd(), 'data/aqueduct_global/aqueduct_global_dl_20150409.gdb'
+    )
+}
+GDB_LAYERS = {
+    'aqueduct_global': 'global_master_20150409'
+}
+
+
+###############################################################################
+## HELPER FUNCTIONS ###########################################################
+###############################################################################
+
+def get_gu(pt, df):
+    """This function maps from a location to a GU (unique identifier in the
+    geodatbase. Intention is to be used in a pandas .apply()
+
+    Arguments:
+      + pt (shapely.Point)[]: this is a shapely Point object representing
+          a geographic location parametrized as (longitude, latitude)
+      + df (geopandas.geodataframe)[]: this is a geodatabase containing the
+          regions, parametrized as shapely.Polygons enclosing a region of
+          interest
+    Returns:
+      + gu (np.array): returns a numpy array of matching GUs corresponding
+          to regions which enclose the given point, pt.
+    """
+    return df[df['geometry'].contains(pt)]['GU'].values
 
 ###############################################################################
 ## ADDRESS TO LAT/LONG ########################################################
@@ -41,7 +73,7 @@ def address_to_loc(addr, api_key=GOOGLE_GEOCODE_API_KEY, return_level=0):
                    with levels 0 and 1
 
     Returns:
-      + loc (tuple): a tuple of floats representing the geolocation
+      + loc (dict): a dictionary containing the geolocation data
     """
     _valid_return_levels = [0, 1, 2]
     assert (return_level in _valid_return_levels), \
@@ -60,11 +92,42 @@ def address_to_loc(addr, api_key=GOOGLE_GEOCODE_API_KEY, return_level=0):
     if return_level == 0:
         return loc
     elif return_level == 1:
-        loc['addr'] = loc.address
+        loc.update({'addr': location.address})
         return loc
     elif return_level == 2:
         loc = location.raw
         return loc
+
+
+###############################################################################
+## LOAD GEODATABASE ###########################################################
+###############################################################################
+
+def load_GDB(gdb_path, layer=None, df_processor=None):
+    """This function loads the geodatabse into a geopandas geodataframe.
+
+    Arguments:
+      + gdb_path (str)[]: string containing the path to the geodatabase to be
+          loaded
+      + layer (str)[None]: string containing the layer to be loaded. Defaults
+          to None to make it an optional parameter in the case that there is
+          only one layer.
+      + df_processor (func)[None]: an optional function to perform some
+          processing on the dataframe before it is returned.
+
+    Returns:
+      + df (geodataframe): geodataframe downloaded from the water resource
+          institute website.
+    """
+    df = geopandas.read_file(gdb_path, layer=layer)
+    if df_processor is not None:
+        df = df_processor(df)
+    return df
+
+###############################################################################
+## JOIN LOCATION AND GEODATABASE ##############################################
+###############################################################################
+
 
 
 
@@ -73,7 +136,12 @@ def address_to_loc(addr, api_key=GOOGLE_GEOCODE_API_KEY, return_level=0):
 ###############################################################################
 
 def main():
-    print()
+    addr = '386 Park Avenue South, New York NY'
+    for ii in range(0,3):
+        location = address_to_loc(addr, return_level=ii)
+        print('Level {}:\n{}\n'.format(ii, location))
+    df = load_GDB(GDBS['aqueduct_global'], GDB_LAYERS['aqueduct_global'])
+
 
 if __name__ == '__main__':
-    main
+    main()
--- tests/test_geocoding.py
+++ tests/test_geocoding.py
@@ -1,4 +1,33 @@
+###############################################################################
+###############################################################################
+##                                                                           ##
+##  test_geocoding.py                                                        ##
+##  Date Created: 2018/12/13                                                 ##
+##                                                                           ##
+##  This set of unit tests is used to ensure the functionality of the        ##
+##  geocoding module.                                                        ##
+##                                                                           ##
+###############################################################################
+###############################################################################
+
 from .context import geocoding
+from geopy.geocoders import GoogleV3
 
 def test_api_key_exists():
     assert geocoding.core.GOOGLE_GEOCODE_API_KEY is not None
+
+def test_api_connection():
+    addr = '386 Park Avenue South, New York, NY 10016'
+    api_key = geocoding.core.GOOGLE_GEOCODE_API_KEY
+    geolocator = GoogleV3(api_key)
+    location = geolocator.geocode(addr)
+    assert location is not None
+
+def test_address_format():
+    addr = '386 Park Avenue South, New York, NY'
+    ret_addr = '386 Park Avenue South, Park Ave S, New York, NY 10016, USA'
+    api_key = geocoding.core.GOOGLE_GEOCODE_API_KEY
+    geolocator = GoogleV3(api_key)
+    location = geolocator.geocode(addr)
+    assert location.address == ret_addr
+
